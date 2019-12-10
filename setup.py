72d279c487e95b2de8af53050260ce3e1f9fff61
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2019-12-10 15:55:06 +0100

    Fix type on setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-        packages=['bybullet_fingers'],
+        packages=['pybullet_fingers'],
         package_dir={'': 'python'}
 )
 
