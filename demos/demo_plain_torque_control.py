4bf883813c2b8baf7a4a919565affae90db1b776
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2020-05-14 16:07:43 +0200

    Fix demo_plain_torque_control.py
    and simplify a little bit

--- demos/demo_plain_torque_control.py
+++ demos/demo_plain_torque_control.py
@@ -14,19 +14,18 @@ if __name__ == "__main__":
         time_step=time_step,
         enable_visualization=True,
         finger_type="single",
-        action_bounds=None,
     )
     # set the finger to a reasonable start position
     finger.reset_finger([0, -0.7, -1.5])
 
     torque = np.array([0.0, 0.3, 0.3])
-    for t in range(10000000):
+    while True:
         time.sleep(time_step)
-
-        # invert the direction of the command every 100 steps
-        if t % 100 == 0:
-            torque *= -1
 
         action = finger.Action(torque=torque)
         t = finger.append_desired_action(action)
         observation = finger.get_observation(t)
+
+        # invert the direction of the command every 100 steps
+        if t % 100 == 0:
+            torque *= -1
