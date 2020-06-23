a318e63773a347097a80dd0f15c1a51151730ee2
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2020-06-23 10:57:47 +0200

    Add another unit test for observation t+1

--- tests/test_action_observation_timing.py
+++ tests/test_action_observation_timing.py
@@ -9,44 +9,60 @@ class TestActionObservationTiming(unittest.TestCase):
     """Test if time relation between action_t and observation_t are correct.
     """
 
-    def test_timing_action_t_vs_observation_t(self):
-        finger = SimFinger(
+    def setUp(self):
+        self.finger = SimFinger(
             time_step=0.001, enable_visualization=False, finger_type="single",
         )
 
-        target_position = [0, -0.7, -1.5]
-        finger.reset_finger(target_position)
+        start_position = [0, -0.7, -1.5]
+        self.finger.reset_finger(start_position)
 
         # Run position control for several iterations to make sure the target
         # position is reached
-        action = finger.Action(position=target_position)
+        action = self.finger.Action(position=start_position)
         for i in range(1000):
-            t = finger.append_desired_action(action)
-            obs = finger.get_observation(t)
+            t = self.finger.append_desired_action(action)
+            obs = self.finger.get_observation(t)
 
-        initial_position = obs.position
+        self.initial_position = obs.position
 
+    def test_timing_action_t_vs_observation_t(self):
         # Apply a max torque action for one step
-        action = finger.Action(
+        action = self.finger.Action(
             torque=[
-                -finger.max_motor_torque,
-                -finger.max_motor_torque,
-                -finger.max_motor_torque,
+                -self.finger.max_motor_torque,
+                -self.finger.max_motor_torque,
+                -self.finger.max_motor_torque,
             ]
         )
-        t = finger.append_desired_action(action)
-        obs = finger.get_observation(t)
+        t = self.finger.append_desired_action(action)
+        obs = self.finger.get_observation(t)
 
         # as obs_t is from just before action_t is applied, the position should
         # not yet have changed
-        np.testing.assert_array_equal(initial_position, obs.position)
+        np.testing.assert_array_equal(self.initial_position, obs.position)
 
         # after applying another action (even with zero torque), we should see
         # the effect
-        t = finger.append_desired_action(finger.Action())
-        obs = finger.get_observation(t)
+        t = self.finger.append_desired_action(self.finger.Action())
+        obs = self.finger.get_observation(t)
         # new position should be less, as negative torque is applied
-        np.testing.assert_array_less(obs.position, initial_position)
+        np.testing.assert_array_less(obs.position, self.initial_position)
+
+    def test_timing_action_t_vs_observation_tplus1(self):
+        # Apply a max torque action for one step
+        action = self.finger.Action(
+            torque=[
+                -self.finger.max_motor_torque,
+                -self.finger.max_motor_torque,
+                -self.finger.max_motor_torque,
+            ]
+        )
+        t = self.finger.append_desired_action(action)
+        obs = self.finger.get_observation(t + 1)
+
+        # new position should be less, as negative torque is applied
+        np.testing.assert_array_less(obs.position, self.initial_position)
 
 
 if __name__ == "__main__":
