75f516244ee5045ad32b4e1832c7cdcb7f5597c2
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2020-07-15 10:51:44 +0200

    Update unit tests

--- tests/test_tasks_move_cube.py
+++ tests/test_tasks_move_cube.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-import random
 import unittest
 import numpy as np
 from scipy.spatial.transform import Rotation
@@ -26,9 +25,8 @@ class TestMoveCube(unittest.TestCase):
                 [+chw, +chw, +chw],
             ]
         )
-        origin_corners = move_cube.get_cube_corner_positions(
-            [0, 0, 0], [0, 0, 0, 1]
-        )
+        origin = move_cube.Pose()
+        origin_corners = move_cube.get_cube_corner_positions(origin)
         np.testing.assert_array_almost_equal(
             expected_origin_corners, origin_corners
         )
@@ -47,7 +45,7 @@ class TestMoveCube(unittest.TestCase):
             ]
         )
         translated = move_cube.get_cube_corner_positions(
-            [1, 2, 3], [0, 0, 0, 1]
+            move_cube.Pose([1, 2, 3], [0, 0, 0, 1])
         )
         np.testing.assert_array_almost_equal(
             expected_translated_corners, translated
@@ -67,7 +65,9 @@ class TestMoveCube(unittest.TestCase):
                 [-chw, +chw, +chw],
             ]
         )
-        rotated = move_cube.get_cube_corner_positions([0, 0, 0], rot_z90)
+        rotated = move_cube.get_cube_corner_positions(
+            move_cube.Pose([0, 0, 0], rot_z90)
+        )
         np.testing.assert_array_almost_equal(expected_rotated_corners, rotated)
 
         # both rotation and translation
@@ -83,15 +83,17 @@ class TestMoveCube(unittest.TestCase):
                 [-chw + 1, +chw + 2, +chw + 3],
             ]
         )
-        both = move_cube.get_cube_corner_positions([1, 2, 3], rot_z90)
+        both = move_cube.get_cube_corner_positions(
+            move_cube.Pose([1, 2, 3], rot_z90)
+        )
         np.testing.assert_array_almost_equal(expected_both_corners, both)
 
     def test_sample_goal_difficulty_1_no_initial_pose(self):
         for i in range(1000):
-            goal_pos, goal_rot = move_cube.sample_goal(difficulty=1)
+            goal = move_cube.sample_goal(difficulty=1)
             # verify the goal is valid (i.e. within the allowed ranges)
             try:
-                move_cube.validate_goal(goal_pos, goal_rot)
+                move_cube.validate_goal(goal)
             except move_cube.InvalidGoalError as e:
                 self.fail(
                     msg="Invalid goal: {}  pose is {}, {}".format(
@@ -100,28 +102,23 @@ class TestMoveCube(unittest.TestCase):
                 )
 
             # verify the goal satisfies conditions of difficulty 1
-
             # always on ground
-            self.assertEqual(goal_pos[2], move_cube._CUBE_WIDTH / 2)
+            self.assertEqual(goal.position[2], move_cube._CUBE_WIDTH / 2)
 
-            # no rotations around x or y axes
-            rx, ry, rz = Rotation.from_quat(goal_rot).as_euler("xyz")
-            self.assertEqual(rx, 0)
-            self.assertEqual(ry, 0)
+            # no orientation
+            np.testing.assert_array_equal(goal.orientation, [0, 0, 0, 1])
 
     def test_sample_goal_difficulty_1_with_initial_pose(self):
         for i in range(1000):
-            initial_pos, initial_rot = move_cube.sample_goal(-1)
+            initial_pose = move_cube.sample_goal(-1)
 
-            goal_pos, goal_rot = move_cube.sample_goal(
-                difficulty=1,
-                current_position=initial_pos,
-                current_orientation=initial_rot,
+            goal = move_cube.sample_goal(
+                difficulty=1, current_pose=initial_pose,
             )
 
             # verify the goal is valid (i.e. within the allowed ranges)
             try:
-                move_cube.validate_goal(goal_pos, goal_rot)
+                move_cube.validate_goal(goal)
             except move_cube.InvalidGoalError as e:
                 self.fail(
                     msg="Invalid goal: {}  pose is {}, {}".format(
@@ -130,26 +127,18 @@ class TestMoveCube(unittest.TestCase):
                 )
 
             # verify the goal satisfies conditions of difficulty 1
-
             # always on ground
-            self.assertEqual(goal_pos[2], move_cube._CUBE_WIDTH / 2)
+            self.assertEqual(goal.position[2], move_cube._CUBE_WIDTH / 2)
 
-            # no rotations around x or y axes
-            expected_rx, expected_ry, expected_rz = Rotation.from_quat(
-                goal_rot
-            ).as_euler("xyz")
-            actual_rx, actual_ry, actual_rz = Rotation.from_quat(
-                goal_rot
-            ).as_euler("xyz")
-            self.assertEqual(actual_rx, expected_rx)
-            self.assertEqual(actual_ry, expected_ry)
+            # no orientation
+            np.testing.assert_array_equal(goal.orientation, [0, 0, 0, 1])
 
     def test_sample_goal_difficulty_2_no_initial_pose(self):
         for i in range(1000):
-            goal_pos, goal_rot = move_cube.sample_goal(difficulty=2)
+            goal = move_cube.sample_goal(difficulty=2)
             # verify the goal is valid (i.e. within the allowed ranges)
             try:
-                move_cube.validate_goal(goal_pos, goal_rot)
+                move_cube.validate_goal(goal)
             except move_cube.InvalidGoalError as e:
                 self.fail(
                     msg="Invalid goal: {}  pose is {}, {}".format(
@@ -158,30 +147,112 @@ class TestMoveCube(unittest.TestCase):
                 )
 
             # verify the goal satisfies conditions of difficulty 2
+            self.assertLessEqual(goal.position[2], move_cube._max_height)
+            self.assertGreaterEqual(goal.position[2], move_cube._min_height)
 
-            half_width = move_cube._CUBE_WIDTH / 2
-            # TODO max_height +/- half_width?
-            self.assertLessEqual(goal_pos[2], move_cube._max_height)
-            self.assertGreaterEqual(goal_pos[2], half_width)
-
-            # no rotations around x or y axes
-            rx, ry, rz = Rotation.from_quat(goal_rot).as_euler("xyz")
-            self.assertEqual(rx, 0)
-            self.assertEqual(ry, 0)
+            # no orientation
+            np.testing.assert_array_equal(goal.orientation, [0, 0, 0, 1])
 
     def test_sample_goal_difficulty_2_with_initial_pose(self):
         for i in range(1000):
-            initial_pos, initial_rot = move_cube.sample_goal(-1)
-
-            goal_pos, goal_rot = move_cube.sample_goal(
-                difficulty=2,
-                current_position=initial_pos,
-                current_orientation=initial_rot,
+            initial_pose = move_cube.sample_goal(-1)
+            goal = move_cube.sample_goal(
+                difficulty=2, current_pose=initial_pose
             )
 
             # verify the goal is valid (i.e. within the allowed ranges)
             try:
-                move_cube.validate_goal(goal_pos, goal_rot)
+                move_cube.validate_goal(goal)
+            except move_cube.InvalidGoalError as e:
+                self.fail(
+                    msg="Invalid goal: {}  pose is {}, {}".format(
+                        e, e.position, e.orientation
+                    ),
+                )
+
+            # verify the goal satisfies conditions of difficulty 2
+            self.assertLessEqual(goal.position[2], move_cube._max_height)
+            self.assertGreaterEqual(goal.position[2], move_cube._min_height)
+
+            # no orientation
+            np.testing.assert_array_equal(goal.orientation, [0, 0, 0, 1])
+
+    def test_sample_goal_difficulty_3_no_initial_pose(self):
+        for i in range(1000):
+            goal = move_cube.sample_goal(difficulty=3)
+            # verify the goal is valid (i.e. within the allowed ranges)
+            try:
+                move_cube.validate_goal(goal)
+            except move_cube.InvalidGoalError as e:
+                self.fail(
+                    msg="Invalid goal: {}  pose is {}, {}".format(
+                        e, e.position, e.orientation
+                    ),
+                )
+
+            # verify the goal satisfies conditions of difficulty 2
+            self.assertLessEqual(goal.position[2], move_cube._max_height)
+            self.assertGreaterEqual(goal.position[2], move_cube._min_height)
+
+            # no orientation
+            np.testing.assert_array_equal(goal.orientation, [0, 0, 0, 1])
+
+    def test_sample_goal_difficulty_3_with_initial_pose(self):
+        for i in range(1000):
+            initial_pose = move_cube.sample_goal(-1)
+            goal = move_cube.sample_goal(
+                difficulty=3, current_pose=initial_pose
+            )
+
+            # verify the goal is valid (i.e. within the allowed ranges)
+            try:
+                move_cube.validate_goal(goal)
+            except move_cube.InvalidGoalError as e:
+                self.fail(
+                    msg="Invalid goal: {}  pose is {}, {}".format(
+                        e, e.position, e.orientation
+                    ),
+                )
+
+            # verify the goal satisfies conditions of difficulty 2
+            self.assertLessEqual(goal.position[2], move_cube._max_height)
+            self.assertGreaterEqual(goal.position[2], move_cube._min_height)
+
+            # no orientation
+            np.testing.assert_array_equal(goal.orientation, [0, 0, 0, 1])
+
+    def test_sample_goal_difficulty_4_no_initial_pose(self):
+        for i in range(1000):
+            goal = move_cube.sample_goal(difficulty=4)
+            # verify the goal is valid (i.e. within the allowed ranges)
+            try:
+                move_cube.validate_goal(goal)
+            except move_cube.InvalidGoalError as e:
+                self.fail(
+                    msg="Invalid goal: {}  pose is {}, {}".format(
+                        e, e.position, e.orientation
+                    ),
+                )
+
+            # verify the goal satisfies conditions of difficulty 2
+            self.assertLessEqual(goal.position[2], move_cube._max_height)
+            self.assertGreaterEqual(goal.position[2], move_cube._min_height)
+
+            # no rotations around x or y axes
+            rx, ry, rz = Rotation.from_quat(goal.orientation).as_euler("xyz")
+            self.assertEqual(rx, 0)
+            self.assertEqual(ry, 0)
+
+    def test_sample_goal_difficulty_4_with_initial_pose(self):
+        for i in range(1000):
+            initial_pose = move_cube.sample_goal(-1)
+            goal = move_cube.sample_goal(
+                difficulty=4, current_pose=initial_pose
+            )
+
+            # verify the goal is valid (i.e. within the allowed ranges)
+            try:
+                move_cube.validate_goal(goal)
             except move_cube.InvalidGoalError as e:
                 self.fail(
                     msg="Invalid goal: {}  pose is {}, {}".format(
@@ -191,35 +262,122 @@ class TestMoveCube(unittest.TestCase):
 
             # verify the goal satisfies conditions of difficulty 2
 
-            half_width = move_cube._CUBE_WIDTH / 2
-            # TODO max_height +/- half_width?
-            self.assertLessEqual(goal_pos[2], move_cube._max_height)
-            self.assertGreaterEqual(goal_pos[2], half_width)
+            self.assertLessEqual(goal.position[2], move_cube._max_height)
+            self.assertGreaterEqual(goal.position[2], move_cube._min_height)
 
             # no rotations around x or y axes
             expected_rx, expected_ry, expected_rz = Rotation.from_quat(
-                goal_rot
+                goal.orientation
             ).as_euler("xyz")
             actual_rx, actual_ry, actual_rz = Rotation.from_quat(
-                goal_rot
+                goal.orientation
             ).as_euler("xyz")
             self.assertEqual(actual_rx, expected_rx)
             self.assertEqual(actual_ry, expected_ry)
 
-    def test_evaluate_state(self):
-        p1 = [0, 0, 0]
-        o1 = [0, 0, 0, 1]
-        p2 = [1, 2, 3]
-        o2 = Rotation.from_euler("z", 0.42).as_quat()
+    def test_evaluate_state_difficulty_1(self):
+        difficulty = 1
+        pose_origin = move_cube.Pose()
+        pose_trans = move_cube.Pose(position=[1, 2, 3])
+        pose_rot = move_cube.Pose(
+            orientation=Rotation.from_euler("z", 0.42).as_quat()
+        )
+        pose_both = move_cube.Pose(
+            [1, 2, 3], Rotation.from_euler("z", 0.42).as_quat()
+        )
 
         # needs to be zero for exact match
-        cost = move_cube.evaluate_state(p1, o1, p1, o1)
+        cost = move_cube.evaluate_state(pose_origin, pose_origin, difficulty)
+        self.assertEqual(cost, 0)
+
+        # None-zero if there is translation, rotation is ignored
+        self.assertNotEqual(
+            move_cube.evaluate_state(pose_origin, pose_trans, difficulty), 0
+        )
+        self.assertEqual(
+            move_cube.evaluate_state(pose_origin, pose_rot, difficulty), 0
+        )
+        self.assertNotEqual(
+            move_cube.evaluate_state(pose_origin, pose_both, difficulty), 0
+        )
+
+    def test_evaluate_state_difficulty_2(self):
+        difficulty = 2
+        pose_origin = move_cube.Pose()
+        pose_trans = move_cube.Pose(position=[1, 2, 3])
+        pose_rot = move_cube.Pose(
+            orientation=Rotation.from_euler("z", 0.42).as_quat()
+        )
+        pose_both = move_cube.Pose(
+            [1, 2, 3], Rotation.from_euler("z", 0.42).as_quat()
+        )
+
+        # needs to be zero for exact match
+        cost = move_cube.evaluate_state(pose_origin, pose_origin, difficulty)
+        self.assertEqual(cost, 0)
+
+        # None-zero if there is translation, rotation is ignored
+        self.assertNotEqual(
+            move_cube.evaluate_state(pose_origin, pose_trans, difficulty), 0
+        )
+        self.assertEqual(
+            move_cube.evaluate_state(pose_origin, pose_rot, difficulty), 0
+        )
+        self.assertNotEqual(
+            move_cube.evaluate_state(pose_origin, pose_both, difficulty), 0
+        )
+
+    def test_evaluate_state_difficulty_3(self):
+        difficulty = 3
+        pose_origin = move_cube.Pose()
+        pose_trans = move_cube.Pose(position=[1, 2, 3])
+        pose_rot = move_cube.Pose(
+            orientation=Rotation.from_euler("z", 0.42).as_quat()
+        )
+        pose_both = move_cube.Pose(
+            [1, 2, 3], Rotation.from_euler("z", 0.42).as_quat()
+        )
+
+        # needs to be zero for exact match
+        cost = move_cube.evaluate_state(pose_origin, pose_origin, difficulty)
+        self.assertEqual(cost, 0)
+
+        # None-zero if there is translation, rotation is ignored
+        self.assertNotEqual(
+            move_cube.evaluate_state(pose_origin, pose_trans, difficulty), 0
+        )
+        self.assertEqual(
+            move_cube.evaluate_state(pose_origin, pose_rot, difficulty), 0
+        )
+        self.assertNotEqual(
+            move_cube.evaluate_state(pose_origin, pose_both, difficulty), 0
+        )
+
+    def test_evaluate_state_difficulty_4(self):
+        difficulty = 4
+        pose_origin = move_cube.Pose()
+        pose_trans = move_cube.Pose(position=[1, 2, 3])
+        pose_rot = move_cube.Pose(
+            orientation=Rotation.from_euler("z", 0.42).as_quat()
+        )
+        pose_both = move_cube.Pose(
+            [1, 2, 3], Rotation.from_euler("z", 0.42).as_quat()
+        )
+
+        # needs to be zero for exact match
+        cost = move_cube.evaluate_state(pose_origin, pose_origin, difficulty)
         self.assertEqual(cost, 0)
 
         # None-zero if there is translation, rotation or both
-        self.assertNotEqual(move_cube.evaluate_state(p1, o1, p2, o1), 0)
-        self.assertNotEqual(move_cube.evaluate_state(p1, o1, p1, o2), 0)
-        self.assertNotEqual(move_cube.evaluate_state(p1, o1, p2, o2), 0)
+        self.assertNotEqual(
+            move_cube.evaluate_state(pose_origin, pose_trans, difficulty), 0
+        )
+        self.assertNotEqual(
+            move_cube.evaluate_state(pose_origin, pose_rot, difficulty), 0
+        )
+        self.assertNotEqual(
+            move_cube.evaluate_state(pose_origin, pose_both, difficulty), 0
+        )
 
     def test_validate_goal(self):
         half_width = move_cube._CUBE_WIDTH / 2
@@ -228,17 +386,23 @@ class TestMoveCube(unittest.TestCase):
 
         # test some valid goals
         try:
-            move_cube.validate_goal([0, 0, half_width], [0, 0, 0, 1])
+            move_cube.validate_goal(
+                move_cube.Pose([0, 0, half_width], [0, 0, 0, 1])
+            )
         except Exception as e:
             self.fail("Valid goal was considered invalid because %s" % e)
 
         try:
-            move_cube.validate_goal([0.05, -0.1, half_width], yaw_rotation)
+            move_cube.validate_goal(
+                move_cube.Pose([0.05, -0.1, half_width], yaw_rotation)
+            )
         except Exception as e:
             self.fail("Valid goal was considered invalid because %s" % e)
 
         try:
-            move_cube.validate_goal([-0.12, 0.0, 0.06], full_rotation)
+            move_cube.validate_goal(
+                move_cube.Pose([-0.12, 0.0, 0.06], full_rotation)
+            )
         except Exception as e:
             self.fail("Valid goal was considered invalid because %s" % e)
 
@@ -246,25 +410,33 @@ class TestMoveCube(unittest.TestCase):
 
         # invalid values
         with self.assertRaises(ValueError):
-            move_cube.validate_goal([0, 0], [0, 0, 0, 1])
+            move_cube.validate_goal(move_cube.Pose([0, 0], [0, 0, 0, 1]))
         with self.assertRaises(ValueError):
-            move_cube.validate_goal([0, 0, 0], [0, 0, 1])
+            move_cube.validate_goal(move_cube.Pose([0, 0, 0], [0, 0, 1]))
 
         # invalid positions
         with self.assertRaises(move_cube.InvalidGoalError):
-            move_cube.validate_goal([0.3, 0, half_width], [0, 0, 0, 1])
+            move_cube.validate_goal(
+                move_cube.Pose([0.3, 0, half_width], [0, 0, 0, 1])
+            )
         with self.assertRaises(move_cube.InvalidGoalError):
-            move_cube.validate_goal([0, -0.3, half_width], [0, 0, 0, 1])
+            move_cube.validate_goal(
+                move_cube.Pose([0, -0.3, half_width], [0, 0, 0, 1])
+            )
         with self.assertRaises(move_cube.InvalidGoalError):
-            move_cube.validate_goal([0, 0, 0.3], [0, 0, 0, 1])
+            move_cube.validate_goal(move_cube.Pose([0, 0, 0.3], [0, 0, 0, 1]))
         with self.assertRaises(move_cube.InvalidGoalError):
-            move_cube.validate_goal([0, 0, 0], [0, 0, 0, 1])
+            move_cube.validate_goal(move_cube.Pose([0, 0, 0], [0, 0, 0, 1]))
         with self.assertRaises(move_cube.InvalidGoalError):
-            move_cube.validate_goal([0, 0, -0.01], [0, 0, 0, 1])
+            move_cube.validate_goal(
+                move_cube.Pose([0, 0, -0.01], [0, 0, 0, 1])
+            )
 
         # valid CoM position but rotation makes it reach out of valid range
         with self.assertRaises(move_cube.InvalidGoalError):
-            move_cube.validate_goal([0, 0, half_width], full_rotation)
+            move_cube.validate_goal(
+                move_cube.Pose([0, 0, half_width], full_rotation)
+            )
 
 
 if __name__ == "__main__":
