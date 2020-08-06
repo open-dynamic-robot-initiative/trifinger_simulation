d9df7daf28ae6ae74000ccda77b3d13077b15bca
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2020-08-06 10:18:35 +0200

    Add unit tests to ensure t < 0 is not accepted

--- tests/test_robot_equivalent_interface.py
+++ tests/test_robot_equivalent_interface.py
@@ -16,17 +16,8 @@ class TestRobotEquivalentInterface(unittest.TestCase):
             finger_type="single", time_step=0.001, enable_visualization=False,
         )
 
-        start_position = [0, -0.7, -1.5]
-        self.finger.reset_finger_positions_and_velocities(start_position)
-
-        # Run position control for several iterations to make sure the target
-        # position is reached
-        action = self.finger.Action(position=start_position)
-        for i in range(1000):
-            t = self.finger.append_desired_action(action)
-            obs = self.finger.get_observation(t)
-
-        self.initial_position = obs.position
+        self.start_position = [0, -0.7, -1.5]
+        self.finger.reset_finger_positions_and_velocities(self.start_position)
 
     def tearDown(self):
         # destroy the simulation to ensure that the next test starts with a
@@ -48,14 +39,14 @@ class TestRobotEquivalentInterface(unittest.TestCase):
 
         # as obs_t is from just before action_t is applied, the position should
         # not yet have changed
-        np.testing.assert_array_equal(self.initial_position, obs.position)
+        np.testing.assert_array_equal(self.start_position, obs.position)
 
         # after applying another action (even with zero torque), we should see
         # the effect
         t = self.finger.append_desired_action(self.finger.Action())
         obs = self.finger.get_observation(t)
         # new position should be less, as negative torque is applied
-        np.testing.assert_array_less(obs.position, self.initial_position)
+        np.testing.assert_array_less(obs.position, self.start_position)
 
     def test_timing_action_t_vs_observation_tplus1(self):
         """Verify that observation_{t+1} is affected by action_t."""
@@ -71,7 +62,7 @@ class TestRobotEquivalentInterface(unittest.TestCase):
         obs = self.finger.get_observation(t + 1)
 
         # new position should be less, as negative torque is applied
-        np.testing.assert_array_less(obs.position, self.initial_position)
+        np.testing.assert_array_less(obs.position, self.start_position)
 
     def test_timing_observation_t_vs_tplus1(self):
         # Apply a max torque action for one step
@@ -139,6 +130,10 @@ class TestRobotEquivalentInterface(unittest.TestCase):
 
     def test_exception_on_old_t(self):
         """Verify that calling get_observation with invalid t raises error."""
+        # verify that t < 0 is not accepted
+        with self.assertRaises(ValueError):
+            self.finger.get_observation(-1)
+
         # append two actions
         t1 = self.finger.append_desired_action(self.finger.Action())
         t2 = self.finger.append_desired_action(self.finger.Action())
@@ -155,6 +150,10 @@ class TestRobotEquivalentInterface(unittest.TestCase):
             self.finger.get_observation(t2 + 2)
 
     def test_get_desired_action(self):
+        # verify that t < 0 is not accepted
+        with self.assertRaises(ValueError):
+            self.finger.get_desired_action(-1)
+
         orig_action = self.finger.Action(
             torque=[1.0, 2.0, 3.0], position=[0.0, -1.0, -2.0]
         )
@@ -165,6 +164,10 @@ class TestRobotEquivalentInterface(unittest.TestCase):
         np.testing.assert_array_equal(orig_action.position, action.position)
 
     def test_get_applied_action(self):
+        # verify that t < 0 is not accepted
+        with self.assertRaises(ValueError):
+            self.finger.get_applied_action(-1)
+
         desired_action = self.finger.Action(torque=[5, 5, 5])
         t = self.finger.append_desired_action(desired_action)
         applied_action = self.finger.get_applied_action(t)
@@ -198,6 +201,10 @@ class TestRobotEquivalentInterface(unittest.TestCase):
         self.assertEqual(stamp_tp1 - stamp_t, 1)
 
     def test_get_timestamp_ms_invalid_t(self):
+        # verify that t < 0 is not accepted
+        with self.assertRaises(ValueError):
+            self.finger.get_timestamp_ms(-1)
+
         t = self.finger.append_desired_action(self.finger.Action())
 
         with self.assertRaises(ValueError):
@@ -207,6 +214,10 @@ class TestRobotEquivalentInterface(unittest.TestCase):
             self.finger.get_timestamp_ms(t + 2)
 
     def test_get_current_timeindex(self):
+        # no time index before first action
+        with self.assertRaises(ValueError):
+            self.finger.get_current_timeindex()
+
         t = self.finger.append_desired_action(self.finger.Action())
         self.assertEqual(self.finger.get_current_timeindex(), t)
 
