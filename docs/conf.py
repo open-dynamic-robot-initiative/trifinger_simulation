0c9694865eafa8596150b73159c4f86794964519
Shruti Joshi <shrutijoshi98@gmail.com>
2020-08-13 10:39:41 +0200

    for api

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../python/trifinger_simulation'))
+sys.path.insert(0, os.path.abspath('../'))
 
 
 # -- General configuration ------------------------------------------------
