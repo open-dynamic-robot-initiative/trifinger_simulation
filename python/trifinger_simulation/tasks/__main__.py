639777f669c422e1d8d7fb501af005a9d2a3f1b7
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2020-09-04 10:17:02 +0200

    __main__ module for tasks package and goal file validation
    This file can be executed from anywhere using
    
        python -m trifinger_simulation.tasks
    
    It serves as a convenient way to call functions to sample goals and
    validate files for the move_cube task.
    
    Add a function `validate_goal_file` which validates a JSON file
    specifying goal parameters for the submission system.

---
+++ python/trifinger_simulation/tasks/__main__.py
@@ -0,0 +1,50 @@
+import argparse
+import sys
+
+from . import move_cube
+
+
+def move_cube_argparse(parser):
+    sub = parser.add_subparsers(title="Commands", dest="command")
+
+    validate_file = sub.add_parser("validate_goal_file")
+    validate_file.add_argument("filename", type=str)
+    validate_file.set_defaults(func=move_cube_validate_goal_file)
+
+    validate_file = sub.add_parser("sample_goal")
+    validate_file.add_argument("difficulty", type=int, choices=[1, 2, 3, 4])
+    validate_file.set_defaults(func=move_cube_sample_goal)
+
+
+def move_cube_validate_goal_file(args):
+    try:
+        move_cube.validate_goal_file(args.filename)
+    except Exception as e:
+        print(e)
+        sys.exit(1)
+
+
+def move_cube_sample_goal(args):
+    try:
+        print(move_cube.sample_goal(args.difficulty).to_json())
+    except Exception as e:
+        print(e)
+        sys.exit(1)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(title="Tasks", dest="task")
+
+    parser_move_cube = subparsers.add_parser("move_cube")
+    move_cube_argparse(parser_move_cube)
+
+    args = parser.parse_args()
+    if hasattr(args, "func"):
+        args.func(args)
+    else:
+        parser.print_help()
+
+
+if __name__ == "__main__":
+    main()
