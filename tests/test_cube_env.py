e37bcb06186a314a1e9be81826593cefa92e015d
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2020-10-06 14:48:28 +0200

    Update the maximum torque
    The max. current on the robot was increased to 2.2 A, so the max. torque
    increases to 0.396 Nm.

--- tests/test_cube_env.py
+++ tests/test_cube_env.py
@@ -20,7 +20,8 @@ class TestSample(unittest.TestCase):
             action = env.action_space.sample()
             observation, _, _, _ = env.step(action)
 
-            self.assertTrue(env.observation_space.contains(observation))
+            self.assertTrue(env.observation_space.contains(observation),
+                            msg="Invalid observation: {}".format(observation))
 
 
 if __name__ == "__main__":
