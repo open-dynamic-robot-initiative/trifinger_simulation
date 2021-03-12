b38d90a8f0e9c304dd370b74ed1f7bee9aaccac4
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2021-03-12 12:35:42 +0100

    Add basic unit tests for Camera classes
    Only simple tests, checking if images of the correct shape are
    generated.  They don't verify the content of the images.

--- tests/test_camera.py
+++ tests/test_camera.py
@@ -3,8 +3,9 @@ import os
 import pathlib
 import pytest
 import numpy as np
+import pybullet
 
-from trifinger_simulation import camera
+from trifinger_simulation import camera as sim_camera
 
 
 TEST_DATA_DIR, _ = os.path.splitext(__file__)
@@ -19,7 +20,7 @@ def test_calib_data_to_matrix_1x1():
     }
     expected = np.array([[42]])
 
-    np.testing.assert_array_equal(camera.calib_data_to_matrix(data), expected)
+    np.testing.assert_array_equal(sim_camera.calib_data_to_matrix(data), expected)
 
 
 @pytest.mark.calib_data_to_matrix
@@ -31,7 +32,7 @@ def test_calib_data_to_matrix_3x3():
     }
     expected = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 
-    np.testing.assert_array_equal(camera.calib_data_to_matrix(data), expected)
+    np.testing.assert_array_equal(sim_camera.calib_data_to_matrix(data), expected)
 
 
 @pytest.mark.calib_data_to_matrix
@@ -43,7 +44,7 @@ def test_calib_data_to_matrix_2x4():
     }
     expected = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
 
-    np.testing.assert_array_equal(camera.calib_data_to_matrix(data), expected)
+    np.testing.assert_array_equal(sim_camera.calib_data_to_matrix(data), expected)
 
 
 def test_camera_parameters_load():
@@ -71,9 +72,9 @@ def test_camera_parameters_load():
     )
 
     with open(config_file, "r") as f:
-        params = camera.CameraParameters.load(f)
+        params = sim_camera.CameraParameters.load(f)
 
-    assert type(params) == camera.CameraParameters
+    assert type(params) == sim_camera.CameraParameters
     assert params.name == "camera180"
     assert params.width == 720
     assert params.height == 540
@@ -89,7 +90,7 @@ def test_camera_parameters_load():
 
 
 def test_load_camera_parameters():
-    params = camera.load_camera_parameters(
+    params = sim_camera.load_camera_parameters(
         pathlib.Path(TEST_DATA_DIR), "camera{id}_full.yml"
     )
 
@@ -99,3 +100,86 @@ def test_load_camera_parameters():
     assert params[0].name == "camera60"
     assert params[1].name == "camera180"
     assert params[2].name == "camera300"
+
+
+def test_camera():
+    pybullet.connect(pybullet.DIRECT)
+
+    width = 150
+    height = 100
+
+    camera = sim_camera.Camera(
+        (0, 0, 0.3),
+        (0, 0, 0, 1),
+        image_size=(width, height),
+    )
+
+    # only some very basic test, verifying that we get an image of the correct
+    # shape
+    image = camera.get_image()
+    assert image.shape == (height, width, 3)
+
+    pybullet.disconnect()
+
+
+def test_calibrated_camera():
+    config_file = os.path.join(TEST_DATA_DIR, "camera180_full.yml")
+
+    pybullet.connect(pybullet.DIRECT)
+
+    with open(config_file) as f:
+        params = sim_camera.CameraParameters.load(f)
+    camera = sim_camera.CalibratedCamera(
+        params.camera_matrix,
+        params.distortion_coefficients,
+        params.tf_world_to_camera,
+        (params.width, params.height),
+        near_plane_distance=0.02,
+        far_plane_distance=1.0,
+    )
+
+    # only some very basic test, verifying that we get an image of the correct
+    # shape
+    image = camera.get_image()
+    assert image.shape == (params.height, params.width, 3)
+
+    pybullet.disconnect()
+
+
+def test_trifingercameras():
+    pybullet.connect(pybullet.DIRECT)
+
+    width = 150
+    height = 100
+    expected_shape = (height, width, 3)
+
+    tricamera = sim_camera.TriFingerCameras(
+        image_size=(width, height),
+    )
+
+    images = tricamera.get_images()
+    assert len(images) == 3
+    assert images[0].shape == expected_shape
+    assert images[1].shape == expected_shape
+    assert images[2].shape == expected_shape
+
+    pybullet.disconnect()
+
+
+def test_create_trifinger_camera_array_from_config():
+    pybullet.connect(pybullet.DIRECT)
+
+    expected_shape = (540, 720, 3)
+
+    tricamera = sim_camera.create_trifinger_camera_array_from_config(
+        pathlib.Path(TEST_DATA_DIR),
+        "camera{id}_full.yml",
+    )
+
+    images = tricamera.get_images()
+    assert len(images) == 3
+    assert images[0].shape == expected_shape
+    assert images[1].shape == expected_shape
+    assert images[2].shape == expected_shape
+
+    pybullet.disconnect()
