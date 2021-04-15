f1fbe67164c661f18a7988fd2b1e5588ea95052a
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2021-04-15 16:47:35 +0200

    Add Gym Env for the move_cube_on_trajectory task
    This is mostly based on the `CubeEnv` with adjustments for the
    trajectory task.
    
    Apart from that some notable changes are:
    
    - Use >= instead of == in is_done check
    - Rename "frameskip" to "step_size"

--- python/trifinger_simulation/gym_wrapper/__init__.py
+++ python/trifinger_simulation/gym_wrapper/__init__.py
@@ -13,3 +13,9 @@ register(
     id="real_robot_challenge_phase_1-v1",
     entry_point="trifinger_simulation.gym_wrapper.envs.cube_env:CubeEnv",
 )
+
+
+register(
+    id="cube_trajectory-v1",
+    entry_point="trifinger_simulation.gym_wrapper.envs.cube_trajectory_env:CubeTrajectoryEnv",
+)
