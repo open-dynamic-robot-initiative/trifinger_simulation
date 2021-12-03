64e7c9ea836704a8d1b0269f8c347eb13be54c94
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2021-12-03 10:43:11 +0100

    doc: Revise docs of SimFinger and add type hints

--- python/trifinger_simulation/__init__.py
+++ python/trifinger_simulation/__init__.py
@@ -1,9 +1,9 @@
 import pathlib
 
 # import some important classes to the main module
-from .sim_finger import SimFinger  # noqa
 from .action import Action  # noqa
 from .observation import Observation  # noqa
+from .sim_finger import SimFinger  # noqa
 from .trifinger_platform import (  # noqa
     TriFingerPlatform,
     ObjectPose,
--- python/trifinger_simulation/observation.py
+++ python/trifinger_simulation/observation.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python3
+import numpy as np
 
 
+# TODO use named tuple for this?
 class Observation:
-    """
-    Robot state observation.
+    """Robot state observation."""
 
-    The length of the attributes depends on the robot type.  In the following
-    ``n_joints`` is the number of joints and ``n_fingers`` the number of
-    fingers (e.g. for the TriFinger robots ``n_fingers = 3, n_joints = 9``).
-
-    Attributes:
-        position (array, shape=(n_joints,)):  Angular joint positions in radian.
-        velocity (array, shape=(n_joints,)):  Joint velocities in rad/s.
-        torque (array, shape=(n_joints,)):  Joint torques in Nm.
-        tip_force (array, shape=(n_fingers,)):  Measurement of the push sensors
-            on the finger tips.
-    """
+    #: Angular joint positions in radian.  Shape = (n_joints,)
+    position: np.ndarray
+    #: Joint velocities in rad/s.  Shape = (n_joints,)
+    velocity: np.ndarray
+    #: Joint torques in Nm.  Shape = (n_joints,)
+    torque: np.ndarray
+    #: Measurement of the push sensors on the finger tips.
+    #: Shape = (n_fingers,)
+    tip_force: np.ndarray
 
     def __init__(self):
         self.position = []
